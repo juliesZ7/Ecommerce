95f37e7ebfe077d28cd170d60dfd3163d5c5e531
Nguyễn Phúc Vĩnh Kỳ 60135881 <62934071+vinhkygm@users.noreply.github.com>
2021-04-24 11:10:05 +0700

    .

--- lib/ProductInfo.dart
+++ lib/ProductInfo.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 
 class ProductInfo {
   final String id;
+  final String image;
   final String name;
   final double price;
   final String description;
-  ProductInfo({this.id, this.name, this.price, this.description});
+  ProductInfo({this.id, this.image, this.name, this.price, this.description});
 }
-
--- lib/ProductScreen.dart
+++ lib/ProductScreen.dart
@@ -12,23 +12,43 @@ class _ProductScreenState extends State<ProductScreen> {
   void initState() {
     super.initState();
     productInfo = ProductInfo(
-        id: 'ID01', name: 'Name', price: 0, description: 'Description');
+        id: 'ID01',
+        image: 'assets/graphics/samsung-galaxy-note-9.jpg',
+        name: 'Samsung Galaxy Note 9',
+        price: 8000000,
+        description: 'Description');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: 80,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.centerLeft,
+                end: Alignment.centerRight,
+                colors: [Colors.blueAccent, Colors.greenAccent])),
+        child: TextButton(
+          child: Text(
+            'Add to cart',
+            style: TextStyle(
+                fontSize: 30, color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          onPressed: () {},
+        ),
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Center(
             child: Container(
                 height: MediaQuery.of(context).size.height * 0.4,
-                child:
-                    Image.asset('assets/graphics/samsung-galaxy-note-9.jpg')),
+                child: Image.asset(productInfo.image)),
           ),
           SizedBox(
-            height: 10,
+            height: 50,
           ),
           Padding(
             padding: const EdgeInsets.only(left: 10),
@@ -43,34 +63,14 @@ class _ProductScreenState extends State<ProductScreen> {
               productInfo.price.toString(),
               style: TextStyle(
                   fontWeight: FontWeight.bold,
-                  fontSize: 70,
+                  fontSize: 30,
                   color: Colors.redAccent),
             ),
           ),
-          
           Padding(
             padding: const EdgeInsets.all(10),
             child: Text(
               productInfo.description,
-            ),
-          ),
-          Container(
-            height: 80,
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.centerLeft,
-                    end: Alignment.centerRight,
-                    colors: [Colors.blueAccent, Colors.greenAccent])),
-            child: TextButton(
-              child: Text(
-                'Add to cart',
-                style: TextStyle(
-                    fontSize: 30,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold),
-              ),
-              onPressed: () {},
             ),
           ),
         ],
